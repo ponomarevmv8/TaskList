d5e467a27de9f310002e54500e70a585da1396f9
Maxim Ponomarev <ponomarevmv8@yandex.ru>
2023-07-25 20:32:14 +0300

    init commit

---
+++ src/test/java/ru/petproject/taskList/TaskListApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.petproject.taskList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
