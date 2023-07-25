62fc3e0feb5cf4f645b8f227eae0f91639124fb0
Maxim Ponomarev <ponomarevmv8@yandex.ru>
2023-07-25 21:40:33 +0300

    feat: Exception
    Description of exception - NotFound, AccessDenied, Mapping

---
+++ src/main/java/ru/petproject/taskList/exception/AccessDeniedException.java
@@ -0,0 +1,8 @@
+package ru.petproject.taskList.exception;
+
+public class AccessDeniedException extends RuntimeException{
+
+    public AccessDeniedException() {
+        super();
+    }
+}
---
+++ src/main/java/ru/petproject/taskList/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package ru.petproject.taskList.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
